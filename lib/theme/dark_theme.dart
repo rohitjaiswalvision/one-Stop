266cc99960febd14c3e54c9eb6987859fd6cc87b
Rohit Jaiswal <rohitjaiswalvisionvivante@gmail.com>
2026-06-22 18:56:41 +0530

    second push

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+ThemeData dark({Color color = const Color(0xFF0067CE)}) => ThemeData(
   fontFamily: AppConstants.fontFamily,
   primaryColor: color,
-  secondaryHeaderColor: const Color(0xFF009f67),
+  secondaryHeaderColor: const Color(0xFFF37324),
   disabledColor: const Color(0xffa2a7ad),
   brightness: Brightness.dark,
   hintColor: const Color(0xFFbebebe),
